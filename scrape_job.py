f6ac1445ac6157cd28c57108766a774ded85e839
pranavbatra30 <70062137+pranavbatra30@users.noreply.github.com>
2023-07-08 22:36:21 +0530

    Update scrape_job.py

--- scrape_job.py
+++ scrape_job.py
@@ -1,5 +1,24 @@
 import asyncio
-from app import scrape_news
+from aiohttp import ClientSession
+from bs4 import BeautifulSoup
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from flask_sqlalchemy import SQLAlchemy
+from dateutil.parser import parse
+import os
+import urllib.parse
+from app import db, NewsItem  
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+
+async def scrape_news():
+    ...
 
 if __name__ == "__main__":
     asyncio.run(scrape_news())
