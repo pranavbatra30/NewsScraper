24f0ea426ef02045133f366f88ef598c9f33adf8
pranavbatra30 <70062137+pranavbatra30@users.noreply.github.com>
2023-07-09 17:51:12 +0530

    Update scrape_job.py

--- scrape_job.py
+++ scrape_job.py
@@ -38,7 +38,8 @@ async def scrape_news():
     urls = [
         "https://www.blackwallstreet-pa.com/feed/",
         "https://www.pennlive.com/arc/outboundfeeds/rss/?outputType=xml",
-        "https://feeds.npr.org/1001/rss.xml"
+        "https://feeds.npr.org/1001/rss.xml",
+        "https://rss.app/feeds/x9ndtFICT24anVzW.xml"
     ]
     responses = await fetch_all(urls)
 
