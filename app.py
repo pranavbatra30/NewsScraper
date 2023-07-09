d0a933cfef065bf8d389cb4378d7e9fd3b4c7c31
pranavbatra30 <70062137+pranavbatra30@users.noreply.github.com>
2023-07-09 23:41:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,35 +104,34 @@ with app.app_context():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    PAGE_SIZE = 15
+    page = request.args.get('page', 1, type=int)  # Get the current page number from the query parameters
     wordcloud_filename = None
     if request.method == 'POST':
         keyword = request.form['keyword'].lower()
-        source = request.form.get('source')  # Get the selected source from the form data
-        if source == 'all':  # If 'all' was selected, don't filter by source
-            related_news = NewsItem.query.filter(NewsItem.all_words.contains(keyword)).all()
-        else:  # Otherwise, filter by the selected source
-            related_news = NewsItem.query.filter(NewsItem.all_words.contains(keyword), NewsItem.source == source).all()
+        source = request.form.get('source')
+        if source == 'all':
+            related_news = NewsItem.query.filter(NewsItem.all_words.contains(keyword))
+        else:
+            related_news = NewsItem.query.filter(NewsItem.all_words.contains(keyword), NewsItem.source == source)
 
-        # Combine keywords from all articles into a single string
-        all_keywords = ' '.join([item.keywords for item in related_news])
-
-        # Generate a word cloud only if there is at least one keyword
+        all_keywords = ' '.join([item.keywords for item in related_news.all()])
         if all_keywords.strip():
-            wordcloud = WordCloud(width = 900, height = 400,
-                        background_color ='#ffffff',
-                        stopwords = None,
-                        min_font_size = 10).generate(all_keywords)
-            # Save the word cloud as an image in a static directory
+            wordcloud = WordCloud(width = 900, height = 400, background_color ='#ffffff', stopwords = None, min_font_size = 10).generate(all_keywords)
             wordcloud_filename = 'wordcloud.png'
             wordcloud.to_file(f'static/{wordcloud_filename}')
-        else:
-            wordcloud_filename = None
 
-        related_news = [item.as_dict() for item in related_news]
-        return render_template('index.html', news=related_news, wordcloud_filename=wordcloud_filename)
+        # Add pagination to your query
+        related_news = related_news.paginate(page, PAGE_SIZE, False)
+
+        next_url = url_for('index', page=related_news.next_num) if related_news.has_next else None
+        prev_url = url_for('index', page=related_news.prev_num) if related_news.has_prev else None
+
+        related_news = [item.as_dict() for item in related_news.items]
+
+        return render_template('index.html', news=related_news, wordcloud_filename=wordcloud_filename, next_url=next_url, prev_url=prev_url)
     else:
         trending_news = NewsItem.query.order_by(NewsItem.published_date.desc()).limit(12).all()
-        #trending_news = NewsItem.query.order_by(NewsItem.published_date.desc()).all()
         trending_news = [item.as_dict() for item in trending_news]
         return render_template('index.html', news=[], trending_news=trending_news, wordcloud_filename=wordcloud_filename)
 
