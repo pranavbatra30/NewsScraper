3fd5c6c03e3ee0e6ab706f154ae7027913eab5f0
pranavbatra30 <70062137+pranavbatra30@users.noreply.github.com>
2023-07-11 21:04:16 +0530

    Update scrape_job.py

--- scrape_job.py
+++ scrape_job.py
@@ -66,17 +66,10 @@ async def scrape_news():
 
             # Tokenize text
             tokens = word_tokenize(article_content)
-
+            
             # Define additional stopwords that you want to ignore
             additional_stopwords = ['npr', 'pennlive', '2023', 'site', 'get', 'said', 'look', 'etc']
-
-            # Remove stopwords, lemmatize, and convert to lowercase
-            stop_words = set(stopwords.words('english') + additional_stopwords)
-            lemmatizer = WordNetLemmatizer()
-            processed_words = [lemmatizer.lemmatize(word.lower()) for word in tokens if not word.lower() in stop_words]
-
-            all_words = ' '.join(processed_words)
-
+            
             # Filter out short and numeric tokens
             tokens = [token for token in tokens if len(token) > 2 and not token.isnumeric()]
     
@@ -85,6 +78,13 @@ async def scrape_news():
     
             # Keep only nouns, adjectives, and verbs
             tokens = [word for word, pos in tagged_tokens if pos.startswith('N') or pos.startswith('J') or pos.startswith('V')]
+
+            # Remove stopwords, lemmatize, and convert to lowercase
+            stop_words = set(stopwords.words('english') + additional_stopwords)
+            lemmatizer = WordNetLemmatizer()
+            processed_words = [lemmatizer.lemmatize(word.lower()) for word in tokens if not word.lower() in stop_words]
+
+            all_words = ' '.join(processed_words)
 
             # Calculate TF-IDF
             vectorizer = TfidfVectorizer(ngram_range=(1, 2))  # Include unigrams and bi-grams
@@ -101,6 +101,48 @@ async def scrape_news():
             if not NewsItem.get_or_create(title=item.title.text, link=item.link.text, published_date=parse(item.pubDate.text), source=source, image=image, all_words=all_words, keywords=top_keywords):
                 news_item = NewsItem.get_or_create(title=item.title.text, link=item.link.text, published_date=parse(item.pubDate.text), source=source, image=image, all_words=all_words, keywords=top_keywords)
 
+async def update_keywords():
+    # Get all news items from the database
+    news_items = NewsItem.query.all()
+
+    # Iterate over all news items
+    for news_item in news_items:
+        # Process article content
+        article_content = news_item.all_words
+
+        # Tokenize text
+        tokens = word_tokenize(article_content)
+
+        # Filter out short and numeric tokens
+        tokens = [token for token in tokens if len(token) > 2 and not token.isnumeric()]
+
+        # Apply POS tagging
+        tagged_tokens = pos_tag(tokens)
+
+        # Keep only nouns, adjectives, and verbs
+        tokens = [word for word, pos in tagged_tokens if pos.startswith('N') or pos.startswith('J') or pos.startswith('V')]
+
+        # Remove stopwords, lemmatize, and convert to lowercase
+        stop_words = set(stopwords.words('english') + additional_stopwords)
+        lemmatizer = WordNetLemmatizer()
+        processed_words = [lemmatizer.lemmatize(word.lower()) for word in tokens if not word.lower() in stop_words]
+
+        # Calculate TF-IDF
+        vectorizer = TfidfVectorizer(ngram_range=(1, 2))  # Include unigrams and bi-grams
+        vectors = vectorizer.fit_transform([' '.join(processed_words)])
+        names = vectorizer.get_feature_names_out()
+        data = vectors.todense().tolist()
+
+        # Get top10 keywords based on tf-idf score
+        tfidf_scores = sorted(list(zip(names, data[0])), key=lambda x: x[1], reverse=True)[:10]
+        top_keywords = ', '.join([word for word, score in tfidf_scores])
+
+        # Update the news item's keywords in the database
+        news_item.keywords = top_keywords
+        db.session.commit()
+
+
 if __name__ == "__main__":
     with app.app_context():
         asyncio.run(scrape_news())
+        asyncio.run(update_keywords())
